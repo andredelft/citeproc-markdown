031819b80628349e59d26e09c1be67896be3227b
André van Delft <andrevandelft@outlook.com>
2021-10-02 16:58:27 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ from setuptools import setup, find_packages
 with open("README.md") as f:
     README = f.read()
 
-with open('requirements.txt') as f:
-    REQUIREMENTS = f.read().splitlines()
-
 setup(
     name="citeproc-markdown",
-    version="0.1",
+    version="0.2",
     description="Citeproc extension for Python markdown",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -17,7 +14,14 @@ setup(
     author_email="andre@delve.nu",
     license=None,
     packages=find_packages(),
-    install_requires=REQUIREMENTS,
+    install_requires=[
+        "pyyaml>=5.4",
+        "requests>=2.26",
+        "ratelimit>=2.2",
+        "markdown>=3.3",
+        "python-decouple>=3.4",
+        "json5>=0.9"
+    ],
     entry_points={
         'markdown.extensions': [
             'citeproc=citeproc_markdown.citeproc:CiteprocExtension'
